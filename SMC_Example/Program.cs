f90104c4bdffce6def42a993ce50c683f1c7913d
Andrew Cortis <Andrew.Cortis@mcast.edu.mt>
2023-10-26 15:29:39 +0200

    Update with RMSE calculation

--- SMC_Example/Program.cs
+++ SMC_Example/Program.cs
@@ -1,16 +1,22 @@
 ﻿using SMC_Example;
 
 // int simulation = 100;
-SMC_Driver driver = new SMC_Driver(new QuarterCircleSimulator());
+SMC_Driver driver = new SMC_Driver(new HalfSquareSimulator());
+    //new QuarterCircleSimulator());
 
-int[] simulations = new int[] { 100, 10000, 1000000 };
+int[] simulations = new int[] { 10000 };
 
 foreach (int simulation in simulations)
 {
-    double area = driver.RunSimulations(simulation);
+    driver.RunSimulations(simulation);
+    double area = driver.EstimateMu();
+    double RMSE = driver.RMSE();
+
     Console.WriteLine($"{simulation} simulations");
-    Console.WriteLine($"The estimated area of the quarter circle, using {simulation} simulations, is {area}");
     // the exact area is Pi/4
+    Console.WriteLine($"The estimated area of the quarter circle, using {simulation} simulations, is {area}");
+    Console.WriteLine($"The RMSE of the simulation is: {RMSE}");
+    
     Console.WriteLine($"The estmated value of Pi is {area * 4}");
     Console.WriteLine();
 }
--- SMC_Example/SMC_Driver.cs
+++ SMC_Example/SMC_Driver.cs
@@ -10,6 +10,7 @@ namespace SMC_Example
     {
         ISimulator simulator;
 
+        List<double> y_list = new List<double>();
 
         public SMC_Driver(ISimulator simulator)
         {
@@ -20,18 +21,36 @@ namespace SMC_Example
         /// Run the simulation n times
         /// </summary>
         /// <param name="n">n is the number of times that the simulation will be executed. It is the "time" used by the SMC</param>
-        public double RunSimulations(int n)
+        public void RunSimulations(int n)
         {
             double y_i = 0;
-            List<double> y_list = new List<double>(n);
+            y_list.Clear();
+            //List<double> y_list = new List<double>(n);
 
             for (int i = 0; i < n; i++)
             {
                 y_i = simulator.Simulate();
                 y_list.Add(y_i);
             }
+        }
 
+        public double EstimateMu()
+        {
+            // this is the value mu hat _n and it is an estimate (hat) of mu using exactly n simulations
             return y_list.Average();
+        }
+
+        // If you are estimating sigma, then this is the estimated RMSE
+        public double RMSE()
+        {
+            //double sigma = 0.4105; // "magic value" - given in slides. Usually in the home assignment you would want to calculate this....
+
+            // estimated sigma if you are unable to obtain the value
+            //double sigma_squared = y_list.Sum(x => (x - y_list.Average()) * (x - y_list.Average())) / y_list.Count;
+
+            double sigma = Math.Sqrt(y_list.Sum(x => (x - y_list.Average()) * (x - y_list.Average()))/y_list.Count);
+
+            return sigma / Math.Sqrt(y_list.Count);
         }
 
     }
